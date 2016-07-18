27d1626c0cda794f23c59f6b87a42e29ba3a2c96
Ricardo Erikson <ricardoerikson@gmail.com>
2016-07-18 01:51:34 -0400

    Updated assertions to Hamcrest style

--- SimplePair/src/test/java/SolutionTest.java
+++ SimplePair/src/test/java/SolutionTest.java
@@ -1,4 +1,9 @@
-import org.junit.Assert;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 
 /**
@@ -9,44 +14,26 @@ public class SolutionTest {
 
     @Test
     public void testNodeCreation() {
-        Node node = new Node(1);
-        Assert.assertEquals(1, node.label);
-        Assert.assertNull(node.parent);
-        Assert.assertTrue(node.children.isEmpty());
+	Node node = new Node(1);
+	assertThat(node.label, is(1));
+	assertThat(node.parent, is(nullValue()));
+	assertThat(node.children, is(empty()));
     }
 
     @Test
     public void testAddNode() {
-        Node node = new Node(2);
-        node.addChild(1);
-        Assert.assertEquals(1, node.children.size());
-        Assert.assertEquals(node.label, node.children.get(0).parent.label);
+	Node node = new Node(2);
+	node.addChild(1);
+	assertThat(node.children, hasSize(1));
+	assertThat(node.children.get(0).parent.label, is(node.label));
     }
 
     @Test
     public void testAddTwoNodes() {
-        Node root = new Node(3);
-        root.addChild(1);
-        root.addChild(2);
-        Assert.assertEquals(2, root.childrenCount());
-    }
-
-    public void testChangePrimitiveTypeValue() {
-        class Tst {
-
-            public int val;
-            public Tst child;
-
-            public Tst(Tst child, int val) {
-                this.child = child;
-                this.val = val;
-            }
-        }
-
-        Tst tst3 = new Tst(null, 3);
-        Tst tst2 = new Tst(tst3, 2);
-        Tst tst1 = new Tst(tst2, 1);
-
+	Node root = new Node(3);
+	root.addChild(1);
+	root.addChild(2);
+	assertThat(root.childrenCount(), is(2));
     }
 
 }
