bc62cd3e23db720e63893cb4c5b841febf6271d3
Ricardo Erikson <ricardoerikson@gmail.com>
2016-07-18 01:50:54 -0400

    Changed format style and added new methods to iterate over the tree

--- SimplePair/src/main/java/Solution.java
+++ SimplePair/src/main/java/Solution.java
@@ -14,25 +14,42 @@ public class Solution {
 }
 
 class Tree {
-    
+
     private Node root;
     private int numberOfNodes;
     private int qualifier;
 
     public Tree(int numberOfNodes, int qualifier) {
-        this.numberOfNodes = numberOfNodes;
-        this.qualifier = qualifier;
+	this.numberOfNodes = numberOfNodes;
+	this.qualifier = qualifier;
     }
 
     public Tree(String line) {
-        String[] parameters = line.split(" ");
-        int numberOfNodes = Integer.parseInt(parameters[0]);
-        int qualifier = Integer.parseInt(parameters[1]);
-        new Tree(numberOfNodes, qualifier);
+	String[] parameters = line.split(" ");
+	int numberOfNodes = Integer.parseInt(parameters[0]);
+	int qualifier = Integer.parseInt(parameters[1]);
+	new Tree(numberOfNodes, qualifier);
     }
-    
-    public boolean addNode(Node node, int parent, int child) {
-        return false;
+
+    public boolean addNode(int parent, int child) {
+	return addNode(this.root, parent, child);
+    }
+
+    private boolean addNode(Node node, int parent, int child) {
+	if (node == null) {
+	    node = new Node(parent);
+	    node.addChild(child);
+	    return true;
+	} else if (node.label == parent) {
+	    node.addChild(child);
+	    return true;
+	}
+
+	for (Node nodeChild : node.children) {
+	    if (addNode(nodeChild, parent, child))
+		return true;
+	}
+	return false;
     }
 
 }
@@ -44,19 +61,19 @@ class Node {
     public List<Node> children;
 
     public Node(int label) {
-        this.label = label;
-        this.children = new LinkedList<Node>();
+	this.label = label;
+	this.children = new LinkedList<Node>();
     }
 
     public Node addChild(int label) {
-        Node childNode = new Node(label);
-        childNode.parent = this;
-        this.children.add(childNode);
-        return childNode;
+	Node childNode = new Node(label);
+	childNode.parent = this;
+	this.children.add(childNode);
+	return childNode;
     }
 
     public int childrenCount() {
-        return this.children.size();
+	return this.children.size();
     }
 
 }
